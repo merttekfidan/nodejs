e54f5a5f2683d1e67f194b595b1a39d0d7098915
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-01-24 22:40:28 +0100

    Create new own module

--- starter/index.js
+++ starter/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate.js');
 /////////////////////////
 /////FILES
 //Blocking, synchronous way
@@ -30,18 +31,7 @@ console.log('Will read file');**/
 
 /////////////////////////
 ///// SERVER
-const replaceTemplate = (temp,product)=>{
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g,product.image);
-    output = output.replace(/{%PRICE%}/g,product.price);
-    output = output.replace(/{%FROM%}/g,product.from);
-    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g,product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g,product.description);
-    output = output.replace(/{%ID%}/g,product.id);
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
-    return output;
-}
+
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-product_card.html`,'utf-8');
@@ -52,12 +42,10 @@ const dataObj = JSON.parse(data);
 
 const server = http.createServer((req,res)=>{
     const {query, pathname} =  url.parse(req.url,true);
-    console.log(query);
     //OVERVIEW PAGE
     if(pathname ==='/' ||pathname === '/overview'){
         res.writeHead(200,{'Content-type':'text/html'})
         const cardsHtml = dataObj.map(el=> replaceTemplate(tempCard,el)).join('');
-        console.log(cardsHtml)
         const output =  tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
         res.end(output);
 
